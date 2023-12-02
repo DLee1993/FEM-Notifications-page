81dfb1b5b60658112850eaf11b3e4040a9b6bfe3
David Lee <Dai180293@gmail.com>
2023-12-02 16:59:36 +0000

    added font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            fontFamily: {},
-            colors: {},
+            fontFamily: {
+                JakartaSans: ["Plus Jakarta Sans", "sans-serif"],
+            },
+            colors: {
+                Red: "hsl(1, 90%, 64%)",
+                Blue: "hsl(219, 85%, 26%)",
+                White: "hsl(0, 0%, 100%)",
+                VeryLightGrayishBlue: "hsl(210, 60%, 98%)",
+                LightGrayishBlue1: "hsl(211, 68%, 94%)",
+                LightBrayishBlue2: "hsl(205, 33%, 90%)",
+                GrayishBlue: "hsl(219, 14%, 63%)",
+                DarkBrayishBlue: "hsl(219, 12%, 42%)",
+                VeryDarkBlue: "hsl(224, 21%, 14%)",
+            },
         },
     },
-    // eslint-disable-next-line no-undef
     plugins: [],
 };
